683e8cfe4d11d65fa0dd441173029105543d2609
zhuosongz <zszhang.stat@gmail.com>
2019-02-01 23:50:55 +1100

    init

---
+++ R/chinaWeather.R
@@ -0,0 +1,13 @@
+#' Define a filename from current date.
+#'
+#' @param date input a date type
+#' @return character a file name
+#' @keywords filename
+#' @export
+#' @examples
+#' filename()
+#' filename(as.Date("20190201",format="%Y%m%d"))
+#
+filename <- function (date = Sys.time()){
+  paste (format(date, "%Y%m%d"), ".csv", sep = "")
+}
